1d734cf20880c6327299c57e109f3779158100dc
Ayush Bhetwal <rujanbhetwal65@gmail.com>
2023-12-06 08:22:18 +0545

    Delete the unwanted Commit

--- mysite/myapp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from myapp import views
-
-urlpatterns = [
-   path("",views.index,name='myapp'),
-   path("about",views.index,name='about'),
-   path("services",views.index,name='myapp')
-]
