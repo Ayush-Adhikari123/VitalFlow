82e9b25443cc4ea09142fb0b211b6d2c094263a7
suvam khadgi <appleb986@gmail.com>
2023-12-08 13:17:06 +0545

    all done

---
+++ Sem3project/myapp/views.py
@@ -0,0 +1,39 @@
+from django.shortcuts import render,HttpResponse
+
+# Create your views here.
+def index(request):
+    context ={
+        'variable':"this is sent"
+    }
+    return render(request,'homepage.html',context)
+
+def about(request):
+    return HttpResponse('This is my about')
+
+def services(request):
+    return HttpResponse('This is my service')
+
+def home(request):
+    return HttpResponse("This is Home Page")
+
+def availabletest(request):
+    return HttpResponse("This is Available Test Page")
+
+def report(request):
+    return HttpResponse("This is Report Page")
+
+def package(request):
+    return HttpResponse("This is Package Page")
+
+def feedback(request):
+    return HttpResponse("This is Feedback Page")
+
+def about(request):
+    return HttpResponse("This is About Page")
+
+def contact(request):
+    return HttpResponse("This is Contact Page")
+
+def adminpage(request):
+    return render(request,'adminpage.html')
+    
