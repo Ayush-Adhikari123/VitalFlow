d910c18edb7f72ecdba524d9b49f789611ca26fb
Ayush Bhetwal <rujanbhetwal65@gmail.com>
2023-12-17 17:25:42 +0545

    Create Report backend

--- Sem3project/myapp/views.py
+++ Sem3project/myapp/views.py
@@ -2,8 +2,10 @@
 
 # views.py
 from django.shortcuts import render,HttpResponse
-from .models import Report ,Report_Detail # Import the Report model
+from myapp.models import Report,Report_Detail
 from .forms import Report_DetailForm  # Import the Report_DetailForm
+import json
+from django.http import JsonResponse
 
 def index(request):
     context ={
@@ -212,7 +214,46 @@ def adminpage(request):
     
     
     
+# def createreport(request):
+
+
+#     if request.method == 'POST':
+
+#         patient_name = request.POST.get('name')
+#         age = request.POST.get('age')
+#         gender = request.POST.get('gender')
+#         address = request.POST.get('address')
+#         lab_no = request.POST.get('lab_no')
+#         contact = request.POST.get('contact')
+#         date = request.POST.get('date')
+#         consultant = request.POST.get('consultant')
+
+#         # Perform basic validation
+      
+
+#         new_report = Report(
+#             patient_Name=patient_name,
+#             age=age,
+#             gender=gender,
+#             address=address,
+#             lab_no=lab_no,
+#             contact=contact,
+#             date=date,
+#             consultant=consultant
+#         )
+#         new_report.save()
+#         return JsonResponse({'message': 'Data received and processed successfully'})
+#     else:
+#       form = Report_DetailForm()
+#       return render(request, 'createReport.html', {'form': form})
+   
+   
+   
+       
+#---------------
+# ----------------------------------------------------------------------------------         
 def createreport(request):
+    
     options = {
         "Complete Blood Count (CBC)": [
           { "text": 'Red Blood Cell Count (RBC)', "reference": '4.5 - 5.5 million', "unit": 'cells/mcL' },
@@ -263,6 +304,7 @@ def createreport(request):
         ]}
 
     if request.method == 'POST':
+      
         patient_name = request.POST.get('name')
         age = request.POST.get('age')
         gender = request.POST.get('gender')
@@ -287,28 +329,115 @@ def createreport(request):
             consultant=consultant
         )
         new_report.save()
-
-  # Save investigation-related data to the database
+      
         test_dropdown = request.POST.get('test_list')
-        if test_dropdown in options:
-            subtests = options[test_dropdown]
-            for subtest in subtests:
-                text = subtest['text']
-                result = request.POST.get(text)  # Updated to match the input field names
-                reference = subtest['reference']
-                unit = subtest['unit']
+        if test_dropdown:
+            
+            if test_dropdown in options:
+                subtests = options[test_dropdown]
+                for subtest in subtests:
+                    text = subtest['text']
+                    # result = request.POST.get(f'{text}_result')
+                    result ="ok"
+                    reference = subtest['reference']
+                    unit = subtest['unit']
 
-                # Create and save Report_Detail instances related to the report
-                new_report_detail = Report_Detail(
-                    report=new_report,
-                    test_list=test_dropdown,
-                    investigation=text,
-                    results=result,  # Updated field name to 'results'
-                    reference_value=reference,
-                    unit=unit
-                )
-                new_report_detail.save()
+                    new_report_detail = Report_Detail(
+                        test_list=test_dropdown,
+                        investigation=text,
+                        results=result,
+                        reference_value=reference,
+                        unit=unit
+                    )
+                    new_report_detail.save()
 
-        return HttpResponse("Report created successfully")  # Redirect or render a different page here
+                return HttpResponse("Report created successfully")
+            else:
+                return HttpResponse("Invalid test selected. ")
+        else:
+           return HttpResponse("empty. ")
     else:
-        return render(request, 'createReport.html', {'options': options})
+      form = Report_DetailForm()
+    return render(request, 'createReport.html')
+
+#-------------------------------------------
+# def createreport(request):
+#     options = {
+#         "Complete Blood Count (CBC)": [
+#           { "text": 'Red Blood Cell Count (RBC)', "reference": '4.5 - 5.5 million', "unit": 'cells/mcL' },
+#           { "text": 'Hemoglobin (Hb)', "reference": '12.0 - 15.5', "unit": 'g/dL' },
+#           { "text": 'Hematocrit (Hct)', "reference": '38.3% - 48.6%', "unit": '' },
+#           { "text": 'White Blood Cell Count (WBC)', "reference": '4,000 - 11,000', "unit": 'cells/mcL' },
+#           { "text": 'Platelet Count', "reference": '150,000 - 450,000', "unit": 'cells/mcL' }
+#         ],
+#         "Blood Glucose": [
+#           { "text": 'Fasting Blood Glucose', "reference": '70 - 99', "unit": 'mg/dL' }
+#         ],
+#         "Lipid Panel": [
+#           { "text": 'Total Cholesterol', "reference": 'Less than 200', "unit": 'mg/dL' },
+#           { "text": 'LDL Cholesterol', "reference": 'Less than 100', "unit": 'mg/dL' },
+#           { "text": 'HDL Cholesterol', "reference": '40 - 60', "unit": 'mg/dL' },
+#           { "text": 'Triglycerides', "reference": 'Less than 150', "unit": 'mg/dL' }
+#         ],
+#         "Liver Function Tests": [
+#           { "text": 'ALT (Alanine Aminotransferase)', "reference": '7 - 56', "unit": 'U/L' },
+#           { "text": 'AST (Aspartate Aminotransferase)', "reference": '5 - 40', "unit": 'U/L' },
+#           { "text": 'ALP (Alkaline Phosphatase)', "reference": '44 - 147', "unit": 'U/L' },
+#           { "text": 'Total Bilirubin', "reference": '0.3 - 1.2', "unit": 'mg/dL' }
+#         ],
+#         "Kidney Function Tests": [
+#           { "text": 'BUN (Blood Urea Nitrogen)', "reference": '7 - 20', "unit": 'mg/dL' },
+#           { "text": 'Serum Creatinine', "reference": '0.6 - 1.3', "unit": 'mg/dL' }
+#         ],
+#         "Electrolytes": [
+#           { "text": 'Sodium', "reference": '135 - 145', "unit": 'mmol/L' },
+#           { "text": 'Potassium', "reference": '3.5 - 5.0', "unit": 'mmol/L' },
+#           { "text": 'Chloride', "reference": '98 - 108', "unit": 'mmol/L' }
+#         ],
+#         "Thyroid Function Tests": [
+#           { "text": 'TSH (Thyroid Stimulating Hormone)', "reference": '0.4 - 4.0', "unit": 'mIU/L' },
+#           { "text": 'FT4 (Free Thyroxine)', "reference": '0.8 - 1.8', "unit": 'ng/dL' }
+#         ],
+#         "Iron Studies": [
+#           { "text": 'Serum Iron', "reference": '65 - 176', "unit": 'µg/dL' },
+#           { "text": 'TIBC (Total Iron Binding Capacity)', "reference": '250 - 450', "unit": 'µg/dL' },
+#           { "text": 'Ferritin', "reference": '12 - 300', "unit": 'ng/mL' }
+#         ],
+#         "C-Reactive Protein (CRP)": [
+#           { "text": '', "reference": 'Less than 0.8', "unit": 'mg/dL' }
+#         ],
+#         "Uric Acid": [
+#           { "text": 'Male', "reference": '3.4 - 7.0', "unit": 'mg/dL' },
+#           { "text": 'Female', "reference": '2.4 - 6.0', "unit": 'mg/dL' }
+#         ]}
+
+#     if request.method == 'POST':
+#         
+
+#   # Save investigation-related data to the database
+#         data = json.loads(request.body)
+#         test_dropdown = request.POST.get('test_list')
+#         if test_dropdown in options:
+#             subtests = options[test_dropdown]
+#             for subtest in subtests:
+#                 text = subtest['text']
+#                 result = data.get(text)  # Updated to match the input field names
+#                 reference = subtest['reference']
+#                 unit = subtest['unit']
+
+#                 # Create and save Report_Detail instances related to the report
+#                 new_report_detail = Report_Detail(
+#                     report=new_report,
+#                     test_list=test_dropdown,
+#                     investigation=text,
+#                     results=result,  # Updated field name to 'results'
+#                     reference_value=reference,
+#                     unit=unit
+#                 )
+#                 new_report_detail.save()
+
+#         return HttpResponse("Report created successfully")  # Redirect or render a different page here
+#     else:
+#         return render(request, 'createReport.html', {'options': options})
+
+
