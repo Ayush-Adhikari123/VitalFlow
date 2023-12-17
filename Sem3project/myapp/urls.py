ee96e3dc90c8cc424add5257399bfc836d5d44da
suvam khadgi <appleb986@gmail.com>
2023-12-17 19:50:44 +0545

    upto date

--- Sem3project/myapp/urls.py
+++ Sem3project/myapp/urls.py
@@ -1,3 +1,4 @@
+# urls.py
 from django.contrib import admin
 from django.urls import path
 from myapp import views
@@ -13,8 +14,10 @@ urlpatterns = [
    path("feedback",views.feedback, name='feedback'),
    path("contact",views.contact, name='contact'),
    path("adminpage",views.adminpage, name='adminpage'),
-   path("viewreport",views.viewreport,name='viewreport'),
    path("createreport",views.createreport,name='createreport'),
-    path("techlogin",views.techlogin, name='techlogin')
+  #  path("createreport",views.create_report_detail,name='create_report_detail'),
+   # path("create_report_detail",views.createreport,name='create_report_detail')
+    path("techlogin",views.techlogin, name='techlogin'),
+    path("packages",views.packages, name='packages'),
 
  ]
