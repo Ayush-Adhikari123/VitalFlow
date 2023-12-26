3aaf65020bec0d173f72e69f1f2d1a45e30fa098
srijan-0 <srijan.shrestha000@gmail.com>
2023-12-26 21:29:15 +0545

    updated forms.py

--- Sem3project/myapp/forms.py
+++ Sem3project/myapp/forms.py
@@ -1,6 +1,7 @@
 #forms.py
 from django import forms
 from .models import Report_Detail  # Import your ReportDetail model here
+from .models import Report
 
 class Report_DetailForm(forms.ModelForm):
     class Meta:
@@ -8,3 +9,7 @@ class Report_DetailForm(forms.ModelForm):
         fields = ['test_list']
 
         # fields=['test_list']# Include all fields or specify the fields you need
+class PatientForm(forms.Form):
+
+    patient_Name = forms.CharField(max_length=255)
+    contact = forms.CharField(max_length=15)
