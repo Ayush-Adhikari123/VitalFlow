21d9e3c73da13f90a7a785751c6d9e6db6249758
Ayush Bhetwal <rujanbhetwal65@gmail.com>
2024-01-04 17:40:03 +0545

    fORGET PASSWORD COMPLETE

--- Sem3project/myapp/models.py
+++ Sem3project/myapp/models.py
@@ -23,3 +23,13 @@ class Report_Detail(models.Model):
 class technicianlogin(models.Model):
     technician_id=models.CharField(max_length=32)
     Password=models.CharField(max_length=16)    
+    
+class TechAdd(models.Model):
+    first_name =models.CharField( max_length=50)
+    middle_name=models.CharField( max_length=50,null=True)
+    last_name=models.CharField( max_length=50)    
+    email =models.EmailField(null=True)
+    contact =models.IntegerField( )
+    password=models.CharField(max_length=50)
+    com_password=models.CharField(max_length=50)        
+    gender =models.CharField( max_length=50)
