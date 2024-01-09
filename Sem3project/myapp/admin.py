00768e834dd9f8e8b9f32793bc8da5d7dd4fe882
Ayush Bhetwal <rujanbhetwal65@gmail.com>
2024-01-09 17:54:55 +0545

    model for contact us

--- Sem3project/myapp/admin.py
+++ Sem3project/myapp/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from myapp.models import TechAdd
+from myapp.models import TechAdd,Contact
 # Register your models here.
 admin.site.register([TechAdd])
+admin.site.register([Contact])
